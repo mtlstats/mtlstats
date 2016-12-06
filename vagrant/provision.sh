d39a8eceb32341a7f186073cb530d7db34a20db6
Jonathan Lamothe <jlamothe1980@gmail.com>
2016-12-06 14:16:40 -0500

    Better Vagrant Configuration

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get -qy install cabal-install hlint
+apt-get -qy install cabal-install ghc-haddock hlint
 export HOME=/home/vagrant
 sudo -u vagrant /vagrant/vagrant/setup.sh
--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,1 +1,2 @@
+cd /vagrant
 cabal update
