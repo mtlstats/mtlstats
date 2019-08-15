77ee289e1f134be72c331dc9d58610c4209561d2
Jonathan Lamothe <jlamothe1980@gmail.com>
2019-08-15 14:53:39 -0400

    missing haddock comments

--- src/Mtlstats.hs
+++ src/Mtlstats.hs
@@ -1,12 +1,17 @@
 module Mtlstats (initState, mainLoop) where
 
 import Control.Monad.Trans.State (StateT)
-import UI.NCurses (Curses)
+import qualified UI.NCurses as C
 
 import Mtlstats.Types
 
-initState :: Curses ProgState
-initState = return ProgState
+-- | Initializes the progran
+initState :: C.Curses ProgState
+initState = do
+  w <- C.defaultWindow
+  C.updateWindow w C.clear
+  return ProgState
 
-mainLoop :: StateT ProgState Curses ()
+-- | Main program loop
+mainLoop :: StateT ProgState C.Curses ()
 mainLoop = return ()
