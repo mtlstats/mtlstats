3fab328e175d508066fb57b1af7111b83d62626f
Jonathan Lamothe <jlamothe1980@gmail.com>
2019-08-20 00:18:23 -0400

    split mainLoop up into eventHandler and draw functions

--- src/Mtlstats/Events.hs
+++ src/Mtlstats/Events.hs
@@ -19,26 +19,16 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 -}
 
-module Mtlstats (initState, mainLoop) where
+module Mtlstats.Events (handleEvent) where
 
-import Control.Monad.Extra (whenM)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.State (StateT, get)
-import Data.Maybe (fromJust)
+import Control.Monad.Trans.State (StateT)
 import qualified UI.NCurses as C
 
-import Mtlstats.Events
 import Mtlstats.Types
-import Mtlstats.UI
 
--- | Initializes the progran
-initState :: C.Curses ProgState
-initState = return newProgState
-
--- | Main program loop
-mainLoop :: StateT ProgState C.Curses ()
-mainLoop = do
-  get >>= lift . draw
-  w <- lift C.defaultWindow
-  whenM (lift (fromJust <$> C.getEvent w Nothing) >>= handleEvent)
-    mainLoop
+-- | Event handler
+handleEvent
+  :: C.Event
+  -- ^ The even being handled
+  -> StateT ProgState C.Curses Bool
+handleEvent _ = return False
--- src/Mtlstats/UI.hs
+++ src/Mtlstats/UI.hs
@@ -19,26 +19,12 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 -}
 
-module Mtlstats (initState, mainLoop) where
+module Mtlstats.UI (draw) where
 
-import Control.Monad.Extra (whenM)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.State (StateT, get)
-import Data.Maybe (fromJust)
 import qualified UI.NCurses as C
 
-import Mtlstats.Events
 import Mtlstats.Types
-import Mtlstats.UI
 
--- | Initializes the progran
-initState :: C.Curses ProgState
-initState = return newProgState
-
--- | Main program loop
-mainLoop :: StateT ProgState C.Curses ()
-mainLoop = do
-  get >>= lift . draw
-  w <- lift C.defaultWindow
-  whenM (lift (fromJust <$> C.getEvent w Nothing) >>= handleEvent)
-    mainLoop
+-- | Drawing function
+draw :: ProgState -> C.Curses ()
+draw _ = return ()
